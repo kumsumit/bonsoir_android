3491dfc3796b24ba23d55d74d81f038e01bf5e83
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-12 15:31:31 +0530

    code updated

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -9,45 +7,52 @@ group = "fr.skyost.bonsoir"
 version = "1.0.0"
 
 repositories {
-        google()
-        mavenCentral()
-    }
-
+    google()
+    mavenCentral()
+}
 
 android {
     namespace = "fr.skyost.bonsoir"
 
     compileSdk = 37
 
+    defaultConfig {
+        minSdk = 24
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_21)
-        }
+    kotlinOptions {
+        jvmTarget = "21"
     }
 
     sourceSets {
         getByName("main") {
-            java.setSrcDirs("src/main/kotlin")
+            java.setSrcDirs(listOf("src/main/kotlin"))
         }
-    }
-
-    defaultConfig {
-        minSdk = 24
     }
 
     testOptions {
         unitTests {
             isIncludeAndroidResources = true
+
             all {
                 it.useJUnitPlatform()
+
                 it.outputs.upToDateWhen { false }
+
                 it.testLogging {
-                    events("passed", "skipped", "failed", "standardOut", "standardError")
+                    events(
+                        "passed",
+                        "skipped",
+                        "failed",
+                        "standardOut",
+                        "standardError"
+                    )
+
                     showStandardStreams = true
                 }
             }
