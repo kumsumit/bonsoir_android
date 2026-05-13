fcfe511496d3ecec7ebdae6e0e2252bdbbe53384
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-13 15:19:37 +0530

    add to save at external added

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.android") version "2.3.21"
 }
 
 group = "fr.skyost.bonsoir"
